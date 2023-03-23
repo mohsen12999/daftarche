b08c6aabfe9d64f7f710378a0144d1b37e2c5631
mohsen1299@gmail.com <mohsen1299@gmail.com>
2023-03-23 21:13:27 +0430

    save input data in local storage.
    save every input key in local storage base on time.

--- src/pages/MyNote.jsx
+++ src/pages/MyNote.jsx
@@ -1,10 +1,21 @@
 import "./MyNote.css"
 
-const MyNote = () => (
-    <div>
-        <textarea className="my-input" placeholder="چی توی ذهنت هست؟">
-        </textarea>
-    </div>
-);
+const MyNote = () => {
+    const input_id = Date.now();
+
+    const saveInput = (value) =>{
+        localStorage.setItem(input_id, value)
+    }
+
+    return (
+        <div>
+            <textarea
+                className="my-input"
+                onChange={(e) => saveInput(e.target.value)}
+                placeholder="چی توی ذهنت هست؟">
+            </textarea>
+        </div>
+    )
+};
 
 export default MyNote;
