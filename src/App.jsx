3b13d634ea203c6580182949885cd34befff5efd
mohsen1299@gmail.com <mohsen1299@gmail.com>
2023-03-23 21:29:24 +0430

    make All Note component due to show all notes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import AllNotes from "./components/AllNotes";
 import MyNote from "./components/MyNote";
 
 function App() {
   return (
     <div className="App">
       <MyNote />
+      <AllNotes />
     </div>
   )
 }
---
+++ src/components/AllNotes.jsx
@@ -0,0 +1,7 @@
+const AllNotes = () => {
+    return (<div>
+        <h3>همه نوشته‌ها</h3>
+    </div>)
+}
+
+export default AllNotes;
