7ec66ed48735db9baec31be8509366874a12faec
mohsen1299@gmail.com <mohsen1299@gmail.com>
2023-03-25 11:09:13 +0430

    sort keys for showing input keys in correct order.

--- src/components/AllNotes.jsx
+++ src/components/AllNotes.jsx
@@ -3,11 +3,12 @@ import "./AllNotes.css"
 const AllNotes = () => {
     const localStorageKeys = Object.keys(localStorage);
     const input_keys = localStorageKeys.filter(Number);
+    const sorted_input_keys = input_keys.sort();
 
     return (<div>
         <h3>همه نوشته‌ها</h3>
 
-        {input_keys.map((input_key)=>(
+        {sorted_input_keys.map((input_key)=>(
             <div key={input_key}>
                 <p className="input-memory">
                 {localStorage[input_key]}
